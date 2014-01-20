ef28c81d3372f062fb4bfb5b856c550e82f8aead
erlkoing <peter24>
2014-01-20 19:04:59 +0100

    zmiana z wypisywania zweryfikowanych emaili na wysylanie emaila z weryfikacja

--- verify_email.py
+++ verify_email.py
@@ -1,13 +1,12 @@
+# wysyla email weryfikujacy pod podane adresy
 import boto.ses
 import properties
 
-conn = boto.ses.connect_to_region(properties.region,
+conn = boto.ses.connect_to_region(properties.email_region,
                                   aws_access_key_id=properties.access_key_id,
                                   aws_secret_access_key=properties.secret_access_key)
 
-print conn.list_verified_email_addresses()
-
-#for i in range(1,10):
-#	email = head + str(i) + tail
-#	print 'verifying email address ' + email
-#	conn.verify_email_address(email)
+for i in range(1, 10):
+    email = properties.head + str(i) + properties.tail
+    print 'verifying email address ' + email
+    conn.verify_email_address(email)
